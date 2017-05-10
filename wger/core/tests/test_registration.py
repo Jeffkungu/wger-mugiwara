8b207f6ca64a1db25c5a8236726117623464e275
Jeffkungu <Jeffkungu@gmail.com>
2017-05-10 12:48:21 +0300

    Fixes test error

--- wger/core/tests/test_registration.py
+++ wger/core/tests/test_registration.py
@@ -58,8 +58,8 @@ class RegistrationTestCase(WorkoutManagerTestCase):
 
         # Fill in the registration form
         registration_data = {'username': 'myusername',
-                             'password1': 'secret',
-                             'password2': 'secret',
+                             'password1': 'secret!23',
+                             'password2': 'secret!23',
                              'email': 'not an email',
                              'g-recaptcha-response': 'PASSED', }
         count_before = User.objects.count()
